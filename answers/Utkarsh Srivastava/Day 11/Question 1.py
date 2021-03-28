42f970204a5e4c6b0d3a48c834418e948c091722
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-03-28 13:16:42 +0530

    Update Question 1.py

--- answers/Utkarsh Srivastava/Day 11/Question 1.py
+++ answers/Utkarsh Srivastava/Day 11/Question 1.py
@@ -1,26 +1,17 @@
 s = input()
 s = s.upper()
+a = len(s)
 c = r = 0
-if(len(s)%2==0):
-    for i in ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-        for j in s:
-            if(i==j):
-                c = c+1
-        if(c%2==1):
-            print("Palindrome does not exists")
-            break
-        c = 0
-    if(c%2==0):
-        print("Palindrome exists")
-else:
-    for i in ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+for i in ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
         for j in s:
             if(i==j):
                 c = c+1
         if(c%2==1):
             r = r+1
         c = 0
-    if(r==1):
-        print("Palindrome exists")
-    else:
-        print("Palindrome does not exists")
+if(r==1 and a%2==1):
+    print("Palindrome exists")
+elif(r==0 and a%2==0):
+    print("Palindrome exists")
+else:
+    print("Palindrome does not exists")
